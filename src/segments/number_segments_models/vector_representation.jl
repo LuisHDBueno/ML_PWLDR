395e1b848d8aa530473b71d8a1ca4578c7b3e383
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2025-11-19 19:31:39 -0300

    Add vector representation

--- src/segments/number_segments_models/vector_representation.jl
+++ src/segments/number_segments_models/vector_representation.jl
@@ -64,3 +64,51 @@ function vector_representation(
     return vcat(V_C, V_B, V_A)
 end
 
+function vector_representation(
+    pwldr::PWLDR,
+    variable_list::Vector{JuMP.VariableRef};
+    n_samples::Int = 100
+)
+
+    dist_list = []
+    for var in variable_list
+        dist_idx, inner_idx = pwldr.uncertainty_to_distribution[variable]
+        dist = pwldr.PWVR_list[dist_idx].distribution
+        push!(dist_list, (idx = dist_idx, distribution = dist))
+    end
+    ABC = pwldr.ldr_model.ext[:_LDR_ABC]
+
+    A = vcat(ABC.Au, ABC.Al, ABC.Ae)
+    V_A = zeros(5)
+    
+    for line in size(A, 1)
+        V_A .+= values(get_statistics(Vector(A[line,1:end])))
+    end
+    V_A = V_A / size(A, 1)
+
+    B = vcat(ABC.Bu, ABC.Bl, ABC.Be)
+    C = ABC.C
+
+    ξ = [1.0]
+    for pwvr in pwldr.PWVR_list
+        push!(ξ, rand(pwvr.distribution))
+    end
+
+    V_B = zeros(5)
+    V_C = zeros(5)
+    for _ in 1:n_samples
+        for var in dist_list
+            sample = rand(var.distribution)
+            ξ[var.dist_idx] = sample
+        end
+        V_B .+= values(get_statistics(B * ξ))
+        V_C .+= values(get_statistics(C * ξ))
+    end
+
+    V_B = V_B/n_samples
+    V_C = V_C/n_samples
+
+    return vcat(V_C, V_B, V_A)
+end
+
+
